73ab03a2054eab7ca7e3dc2b9ff94bbbfeef0791
Cédric Tran-Xuan <cedric.tran-xuan@forgerock.com>
2018-12-30 18:29:53 +0100

    feat(requirement 1): first implementation of requirement 1.

---
+++ src/main/java/cara/dojo/kata/greeting/GreetService.java
@@ -0,0 +1,7 @@
+package cara.dojo.kata.greeting;
+
+public final class GreetService {
+    public String greet(final String name) {
+        return "Hello, Bob";
+    }
+}
---
+++ src/test/java/cara/dojo/kata/greeting/GreetingShould.java
@@ -0,0 +1,30 @@
+package cara.dojo.kata.greeting;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("Greeting should")
+class GreetingShould {
+    private static GreetService greetService;
+
+    @BeforeAll
+    static void setUp() {
+        greetService = new GreetService();
+    }
+
+    @Test
+    @DisplayName("return 'Hello, <name>' (requirement 1)")
+    void return_greeting_with_hello() {
+        final String greeting = greetService.greet("Bob");
+        assertThat(greeting).isEqualTo("Hello, Bob");
+    }
+}
